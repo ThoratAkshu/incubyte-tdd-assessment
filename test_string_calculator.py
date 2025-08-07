374c64edead9e510daf29b675c4689698ad850cb
ThoratAkshu <86485909+ThoratAkshu@users.noreply.github.com>
2025-08-07 10:28:54 +0530

    Create test_string_calculator.py

---
+++ test_string_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+from string_calculator import add
+
+class TestStringCalculator(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(add(""), 0)
+
+    def test_one_number(self):
+        self.assertEqual(add("1"), 1)
+
+    def test_two_numbers(self):
+        self.assertEqual(add("1,2"), 3)
+
+    def test_new_lines(self):
+        self.assertEqual(add("1\n2,3"), 6)
+
+    def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
+
+    def test_negative_numbers(self):
+        with self.assertRaises(ValueError) as e:
+            add("1,-2,-3")
+        self.assertEqual(str(e.exception), "negative numbers not allowed -2, -3")
+
+if __name__ == "__main__":
+    unittest.main()
